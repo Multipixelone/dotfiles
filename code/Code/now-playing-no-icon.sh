1fa07c6c8b1b593cd24e66c0847c98d60c24bea4
Multipixelone <finn@cnwr.net>
2019-07-07 14:40:04 -0700

    Fix shebang

--- code/Code/now-playing-no-icon.sh
+++ code/Code/now-playing-no-icon.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+MPD_HOST="musicbox"
+
 if ! mpc >/dev/null 2>&1; then
-  echo connection failed
+  echo no mpd
   exit 1
 elif mpc status | grep -q playing; then
   ( zscroll -n -u -b "x" -d 0.5 -l 50 -M "mpc status" -m "playing" -b " " -s 1 -m "paused" -b " " -s 0 "mpc current" ) &
