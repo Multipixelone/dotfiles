bf12fd2e1e6a909e5d561b56877d6a584d8ca953
Multipixelone <finn@cnwr.net>
2019-07-07 14:22:55 -0700

    Add google-auth venv

---
+++ code/Code/google-auth/include/python3.7m/eval.h
@@ -0,0 +1,1 @@
+/nix/store/gyffk0z9rwcdl1l1654z3b0sry98ddrk-python3-3.7.3/include/python3.7m/eval.h
---
+++ code/Code/google-auth/lib/python3.7/encodings/cp856.py
@@ -0,0 +1,1 @@
+/nix/store/gyffk0z9rwcdl1l1654z3b0sry98ddrk-python3-3.7.3/lib/python3.7/encodings/cp856.py
