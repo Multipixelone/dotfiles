be6435519cbb552e18e34ef27750250f0dd1074b
Multipixelone <finn@cnwr.net>
2019-08-05 22:59:49 -0700

    Remove google-auth

--- code/Code/now-playing-no-icon.sh
+++ code/Code/now-playing-no-icon.sh
@@ -6,7 +6,9 @@ if ! mpc >/dev/null 2>&1; then
   echo no mpd
   exit 1
 elif mpc status | grep -q playing; then
-  ( zscroll -n -u -b "x" -d 0.5 -l 50 -M "mpc status" -m "playing" -b " " -s 1 -m "paused" -b " " -s 0 "mpc current" ) &
+zscroll -u true -b "♪ x" -d 0.3 -M "mpc status" -m "playing" \
+		"-b ' '" -m "paused" "-b ' ' -s false" "mpc current" &
+  #( zscroll -n -u -b "x" -d 0.5 -l 50 -M "mpc status" -m "playing" -b " " -s 1 -m "paused" -b " " -s 0 "mpc current" ) &
 else
   echo  no song
 fi
